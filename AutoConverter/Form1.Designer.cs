c82a97de7a60a7bda80ff2dea10c6252384604f3
MJ.Pournassari <Mjpournassari@gmail.com>
2015-09-28 16:50:46 +0330

    *Init

---
+++ AutoConverter/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace AutoConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FileName";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(731, 195);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(13, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(380, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "0%";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(248, 9);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(300, 30);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(591, 9);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "FPS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 271);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AutoConverter 20150928";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AutoConverter/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoConverter
+{
+    public partial class Form1 : Form
+    {
+        string _fileIn = "";
+        string _fileOut = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label2.Text = DateTime.Now.ToString();
+            string[] files = Directory.GetFiles(ConfigurationSettings.AppSettings["Input"].ToString().Trim());
+            foreach (var item in files)
+            {
+                _fileIn = item;
+                label1.Text = _fileIn;
+                _fileOut = ConfigurationSettings.AppSettings["Output"].ToString().Trim() + Path.GetFileNameWithoutExtension(_fileIn);
+
+                //if (!File.Exists(_fileOut+ ConfigurationSettings.AppSettings["Extention"].ToString().Trim()))
+                //{
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = Path.GetDirectoryName(Application.ExecutablePath) + "\\ffmpeg";
+
+                    proc.StartInfo.Arguments = "-i " + "\"" + _fileIn + "\"" + "  -r 25 -b "
+                                                + ConfigurationSettings.AppSettings["MaxBitrate"].ToString().Trim() + "k   -minrate  "
+                                                + ConfigurationSettings.AppSettings["MinBitrate"].ToString().Trim() + "k -maxrate "
+                                                + ConfigurationSettings.AppSettings["MaxBitrate"].ToString().Trim()
+                                                + "k -c:v "
+                                                + ConfigurationSettings.AppSettings["Codec"].ToString().Trim()
+                                                + "  -y  " + "\"" + _fileOut
+                                                + ConfigurationSettings.AppSettings["Extention"].ToString().Trim() + "\"";
+
+                    proc.StartInfo.RedirectStandardError = true;
+                    proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.CreateNoWindow = true;
+                    proc.EnableRaisingEvents = true;
+                    proc.Exited += new EventHandler(myProcess_Exited);
+                    proc.Start();
+                    StreamReader reader = proc.StandardError;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (richTextBox1.Lines.Length > 5)
+                        {
+                            richTextBox1.Text = "";
+                        }
+
+                        FindDuration(line);
+                        richTextBox1.Text += (line) + " \n";
+                        richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                        richTextBox1.ScrollToCaret();
+                        Application.DoEvents();
+                    }
+                }
+                try
+                {
+                    File.Delete(_fileIn);
+                }
+                catch
+                {
+
+                }
+           // }
+            timer1.Enabled = true;
+        }
+        private void myProcess_Exited(object sender, EventArgs e)
+        {
+        }
+        protected void FindDuration(string Str)
+        {
+            try
+            {
+                string TimeCode = "";
+                if (Str.Contains("Duration:"))
+                {
+                    TimeCode = Str.Substring(Str.IndexOf("Duration: "), 21).Replace("Duration: ", "").Trim();
+                    string[] Times = TimeCode.Split('.')[0].Split(':');
+                    double Frames = double.Parse(Times[0].ToString()) * (3600) * (25) +
+                        double.Parse(Times[1].ToString()) * (60) * (25) +
+                        double.Parse(Times[2].ToString()) * (25);
+                    progressBar1.Maximum = int.Parse(Frames.ToString());
+
+
+                }
+                if (Str.Contains("time="))
+                {
+                    try
+                    {
+                        string CurTime = "";
+                        CurTime = Str.Substring(Str.IndexOf("time="), 16).Replace("time=", "").Trim();
+                        string[] CTimes = CurTime.Split('.')[0].Split(':');
+                        double CurFrame = double.Parse(CTimes[0].ToString()) * (3600) * (25) +
+                            double.Parse(CTimes[1].ToString()) * (60) * (25) +
+                            double.Parse(CTimes[2].ToString()) * (25);
+                        progressBar1.Value = int.Parse(CurFrame.ToString());
+                        label3.Text = ((progressBar1.Value * 100) / progressBar1.Maximum).ToString() + "%";
+                        Application.DoEvents();
+                    }
+                    catch
+                    { }
+
+                }
+                if (Str.Contains("fps="))
+                {
+                    string Speed = "";
+                    Speed = Str.Substring(Str.IndexOf("fps="), 8).Replace("fps=", "").Trim();
+                    label4.Text = "Speed: " + (float.Parse(Speed) / 25).ToString() + " X ";
+                    Application.DoEvents();
+                }
+            }
+            catch { }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
